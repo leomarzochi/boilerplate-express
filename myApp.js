95efe58745484b25f841340d8239f04b463e1a3f
Leonardo Marzochi <leomarzochi@live.com>
2022-06-24 17:33:20 -0300

    hello world

--- myApp.js
+++ myApp.js
@@ -1,39 +1,10 @@
-let express = require('express');
+let express = require("express");
 let app = express();
 
+console.log("Hello World");
 
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app;
