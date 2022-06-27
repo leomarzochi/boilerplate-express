55a24318e5bb94d3d351a810611eec3dff9f7aef
Leonardo Marzochi <leomarzochi@live.com>
2022-06-27 20:23:26 -0300

    lesson env

--- myApp.js
+++ myApp.js
@@ -9,4 +9,11 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/json", (req, res) => {
+  if ((process.env.MESSAGE_STYLE = "uppercase")) {
+    res.json({ message: "Hello json".toUpperCase() });
+  }
+  res.json({ message: "Hello json" });
+});
+
 module.exports = app;
